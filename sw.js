a2c6f174ca79cc0901e5a50110462c9c1ecbe382
krsf-work <mdesai.india@gmail.com>
2026-06-16 08:15:02 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE='krsf-v1';
+const CORE=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(CORE);}).then(function(){return self.skipWaiting();}));});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){if(k!==CACHE)return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
+self.addEventListener('fetch',function(e){
+  var req=e.request; if(req.method!=='GET')return;
+  var h=req.url;
+  if(h.indexOf('firestore.googleapis.com')>-1||h.indexOf('identitytoolkit')>-1||h.indexOf('securetoken')>-1||h.indexOf('firebaseinstallations')>-1){return;}
+  if(req.mode==='navigate'){
+    e.respondWith(fetch(req).then(function(res){var c=res.clone();caches.open(CACHE).then(function(cc){cc.put('./index.html',c);});return res;}).catch(function(){return caches.match('./index.html');}));
+    return;
+  }
+  e.respondWith(caches.match(req).then(function(cached){
+    var net=fetch(req).then(function(res){if(res&&(res.status===200||res.type==='opaque')){var c=res.clone();caches.open(CACHE).then(function(cc){cc.put(req,c);});}return res;}).catch(function(){return cached;});
+    return cached||net;
+  }));
+});
